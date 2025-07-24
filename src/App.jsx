ae87316f029ddf24be9455a4758c9ad4e1292655
Nitesh <niteshtyagi149@gmail.com>
2025-07-25 03:08:58 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Quiz from './components/Quiz/Quiz.jsx';
+const App = () => {
+
+return <>
+  <Quiz></Quiz>
+</>
+}
+
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "What does CPU stand for?",
+    option1: "Central Process Unit",
+    option2: "Computer Personal Unit",
+    option3: "Central Processing Unit",
+    option4: "Central Processor Utility",
+    ans: 3
+  },
+  {
+    question: "Which of the following is an input device?",
+    option1: "Monitor",
+    option2: "Printer",
+    option3: "Keyboard",
+    option4: "Speaker",
+    ans: 3
+  },
+  {
+    question: "What is the main function of RAM?",
+    option1: "To store data permanently",
+    option2: "To run applications temporarily",
+    option3: "To connect to the internet",
+    option4: "To back up files",
+    ans: 2
+  },
+  {
+    question: "Which language is primarily used for web development?",
+    option1: "Python",
+    option2: "C++",
+    option3: "JavaScript",
+    option4: "Java",
+    ans: 3
+  },
+  {
+    question: "What does 'OS' stand for in computing?",
+    option1: "Open Software",
+    option2: "Operating System",
+    option3: "Online Server",
+    option4: "Optical Storage",
+    ans: 2
+  }
+]
